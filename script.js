d02062f8eba4d058d6e0382c4ee77f43deec145c
Glitch (zap-) <none>
2018-05-25 08:45:38 +0000

    zap.

---
+++ script.js
@@ -0,0 +1,38 @@
+function onGranted(){
+  Mousetrap.bind(['command+c', 'ctrl+c'], function(e) {
+    var q = window.getSelection().toString();
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=1&q='+q, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+
+        var data = JSON.parse(request.responseText);
+        Push.create(data["Heading"], {
+            body: data["Abstract"],
+            icon: data["Image"],
+            timeout: 4000,
+            onClick: function () {
+                window.open(data["AbstractURL"])
+                this.close();
+            }
+        });
+      } else {
+        // We reached our target server, but it returned an error
+
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+
+    request.send();
+  });
+}
+function onDenied(){}
+
+function demo(){
+  console.log("demo")
+  Push.Permission.request(onGranted, onDenied);
+}
